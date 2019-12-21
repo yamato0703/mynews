1d7676304482fbfee25139e747b5a27ba57e03e1
yamato0703 <omata.yamato.07034649@docomo.me.jp>
2019-12-21 09:50:47 +0000

    review fix

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,3 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/create','Admin\ProfileController@add');
     Route::get('profile/edit','Admin\ProfileController@edit');
 });
-Route::group(['prefix' => 'admin'], function() {
-
-});
