dcfee20c6c0f50bb970a169ce3d9b34d05ca432c
yamato0703 <omata.yamato.07034649@docomo.me.jp>
2020-02-08 06:23:36 +0000

    heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         if (\App::environment('production')) {
-        \URL::forceScheme('https');
-    }
+            \URL::forceScheme('https');
+        }
     }
 }
